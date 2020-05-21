fdd3bf5865a3c60c65ef386f7028d5cd23513b95
Willem Sonke <willem@wimiso.nl>
2020-05-21 11:23:33 +0200

    Update from Eana Eltu, 2020-05-21

--- aysä'o/eana-eltu/kanom.sh
+++ aysä'o/eana-eltu/kanom.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 wget https://tirea.learnnavi.org/dictionarydata/NaviData_fwew.sql
+echo "drop database test;" | mysql
 echo "create database test;" | mysql
 mysql test < NaviData_fwew.sql
 rm NaviData_fwew.sql
@@ -9,3 +10,6 @@ echo "select * from metaWords;" | mysql --batch test > metaWords.tsv
 echo "select * from localizedWords;" | mysql --batch test > localizedWords.tsv
 echo "select * from sources;" | mysql --batch test > sources.tsv
 #echo "drop database test;" | mysql --batch test > metaWords.tsv
+
+rm -rf output
+
